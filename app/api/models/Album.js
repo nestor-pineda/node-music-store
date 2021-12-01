0794b4e58bdfa3f93687bd6bb94be00b4d3eaa34
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-12-01 16:09:42 +0100

    Server creado, modelo Album creado,

---
+++ app/api/models/Album.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema(
+  {
+    name: { type: String, require: true },
+    group: { type: String, require: true },
+    year: { type: String, require: true },
+    gender: { type: String, require: true },
+  },
+  { timestamps: true }
+);
+
+const Album = mongoose.model("albums", AlbumSchema);
+module.exports = Album;
---
+++ index.js
@@ -0,0 +1,62 @@
+// Main imports
+const express = require("express");
+const logger = require("morgan");
+// DB conexion import
+const { connect } = require("./app/config/database");
+// Routes imports
+const albums = require("./app/api/routes/albums.routes");
+// Other imports
+const HTTPSTATUSCODE = require("./app/utils/httpStatusCode");
+const cors = require("cors");
+
+// DB connection
+connect();
+
+const app = express();
+
+// Headers definition
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+// Allowed PSI urls
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:4200"],
+    credentials: true,
+  })
+);
+
+// JSON body format information
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Morgan petitions especification
+app.use(logger("dev"));
+
+// Routes import definition
+// app.use("/albums", albums);
+
+// Manage Error messages
+app.use((req, res, next) => {
+  let err = new Error();
+  err.status = 404;
+  err.message = HTTPSTATUSCODE[404];
+  next(err);
+});
+
+// Handle errors
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json(err.message || "Unexpected error");
+});
+
+// Hides we are usnig Node
+app.disable("x-powered-by");
+
+// Server listening to port 3000
+app.listen(3000, () => {
+  console.log("Node server listening on port 3000");
+});
